feac1af00f751c0c5702b7c7bf3aa9836f0aff59
Megan <petrusomc@appstate.edu>
2018-02-09 11:36:54 -0500

    Updates First Connection Files
    Now shows and updates all information in the database from phpmyadmin

--- connection.php
+++ connection.php
@@ -1,9 +1,22 @@
 <?php
-	$servername = "localhost";
-	$dbname = "test";
 
-	$mysqli_conn = new mysqli($servername, $dbname);
-	if ($mysqli_conn->connect_error) {
-    		die("Connection failed: " . $mysqli_conn->connect_error);
+	$user = 'root';
+	$password = 'root';
+	$db = 'test';
+	$host = 'localhost';
+	$port = 3306;
+
+	$link = mysqli_init();
+	$success = mysqli_real_connect(
+		$link, 
+		$host, 
+		$user, 
+		$password, 
+		$db,
+		$port
+	);
+	
+	if ($success->connect_error) {
+    		die("Connection failed: " . $success->connect_error);
 	}
 ?>
--- firstDBaccess.php
+++ firstDBaccess.php
@@ -1,18 +1,21 @@
 <?php
 	include("connection.php");
 	
-	$sql = "SELECT name, age FROM helloworld";
+	$sql = "SELECT * FROM helloworld";
 	
-	$result = mysqli_query($mysqli_conn, $sql);
-	$length = 7;
+	$result = mysqli_query($link, $sql);
+	$length =10;
 	$row = mysqli_fetch_assoc($result);
 
 	echo "<table>";
+	
 	if (mysqli_num_rows($result) > 0) {
-			echo "<tr><th>Name</th><th>Age</th></tr>";
+			echo "<tr><th>Name</th><th>Age</th><th>Color</th><th>Animal</th></tr>";
 		for ($i = 0; $i < $length; $i++) {
 			echo "<tr><td>" . $row['name'] . 
 			"</td><td>" . $row['age'] . 
+			"</td><td>" . $row['color'] .
+			"</td><td>" . $row['animal'] .
 			"</td></tr>";
 			$row = mysqli_fetch_assoc($result);
 		}
@@ -20,6 +23,6 @@
     		echo "0 results";
 	}
 	echo "</table>";
-	$mysqli_conn->close();
+	$link->close();
 
 ?>
